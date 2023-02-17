51f8f64b606a890d06a2f422397be03d8a1fdd05
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-17 07:51:37 +0000

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.sendFile(path.join(__dirname,'./index.html')),
+  res.sendFile(path.join(__dirname,'../router/index.html'))
 
 
-return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
+   //return res.status(200).json({
+    //title: "Express Testing",
+    //message: "The app is working properly!",
     
-  });
+  //});
 });
 
 module.exports = router;
