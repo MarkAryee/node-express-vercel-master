900b17a4b4cc5939fb9e7af41c0503992c42c4fd
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-17 07:46:08 +0000

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -5,6 +5,7 @@ router.get("/", async (req, res, next) => {
   return res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly!",
+    res.sendFile(path.join(__dirname,'./index.html'))
   });
 });
 
