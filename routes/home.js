ca48ecead65104e2f0ea24709862a71026641cd1
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-17 08:38:59 +0000

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const user = require('../routes/User');
 const path = require('path');
+const mongoose = require('mongoose');
 
-//let fs = require('fs');
+const doURI = "mongodb+srv://markdb:rasengan@cluster0.ad3usrv.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(doURI, {useNewUrlParser: true, useUnifiedTopology: true}).then((result) => {
+  //app.listen(3000);
+}).catch((err) => {
+  console.log('there is an error');
+})
+console.log('something is happening here');
+
 
 router.get("/", async (req, res, next) => {
   res.sendFile(path.join(__dirname,'../routes/index.html'))
@@ -19,5 +28,23 @@ router.get("/", async (req, res, next) => {
    //    res.end();
  // });
 });
+
+router.post('/add-Post', (req, res) => {
+  
+    S_name = req.body.Name;
+    S_id = req.body.ST_ID;
+
+  const User = new user({
+    name: S_name,
+    id: S_id
+  });
+
+  User.save().then((result) => {
+    res.send(result)
+  }).catch((err) => {
+    console.log(err);
+  })
+});
+
 
 module.exports = router;
