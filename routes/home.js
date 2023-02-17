3d2b1f16876e3ead4f83aeb430cced6374f9c52d
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-17 09:33:35 +0000

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-//const user = require('/routes/User');
+const user = require('/routes/User');
 const path = require('path');
 const mongoose = require('mongoose');
 
