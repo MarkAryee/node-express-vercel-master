d2f8e2ef257b9d9d88813750f1df3a428d37014f
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-17 10:01:35 +0000

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -7,24 +7,12 @@ const mongoose = require('mongoose');
 ///*
 const doURI = "mongodb+srv://markdb:rasengan@cluster0.ad3usrv.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(doURI, {useNewUrlParser: true, useUnifiedTopology: true}).then((result) => {
-  //app.listen(3000);
+ 
 }).catch((err) => {})
 //*/
 
 router.get("/add-Get", async (req, res, next) => {
   res.sendFile(path.join(__dirname,'../routes/index.html'))
-
-
-   //return res.status(200).json({
-    //title: "Express Testing",
-    //message: "The app is working properly!",
-    
-  //});
-
-   // fs.readFile('./index.html', null, function (error, data) {
-     //  res.write(data);
-   //    res.end();
- // });
 });
 
 ///*
