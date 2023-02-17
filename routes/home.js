db996c45cac02c97620553d0aed531443e3678e4
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-17 08:23:09 +0000

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const path = require('path');
 
-let fs = require('fs');
+//let fs = require('fs');
 
 router.get("/", async (req, res, next) => {
-  //res.sendFile(path.join(__dirname,'/index.html'))
+  res.sendFile(path.join(__dirname,'/routes/index.html'))
 
 
    //return res.status(200).json({
@@ -13,10 +14,10 @@ router.get("/", async (req, res, next) => {
     
   //});
 
-    fs.readFile('./index.html', null, function (error, data) {
-       res.write(data);
-       res.end();
-    });
+   // fs.readFile('./index.html', null, function (error, data) {
+     //  res.write(data);
+   //    res.end();
+ // });
 });
 
 module.exports = router;
