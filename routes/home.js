92f3b38dbeed019ae270f0451dadaaf1f024de8d
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-18 07:53:52 +0000

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 
 ///*
-const doURI = "mongodb+srv://markdb:rasengan@cluster0.ad3usrv.mongodb.net/b1?retryWrites=true&w=majority"
+const doURI = "mongodb+srv://markdb:rasengan@cluster0.ad3usrv.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(doURI, {useNewUrlParser: true, useUnifiedTopology: true}).then((result) => {
 // app.listen(3000);
 }).catch((err) => {})
@@ -20,6 +20,10 @@ router.get("/add-Get", async (req, res, next) => {
     var S_id;
 router.post('/add-Post', (req, res) => {
   
+mongoose.connect(doURI, {useNewUrlParser: true, useUnifiedTopology: true}).then((result) => {
+// app.listen(3000);
+}).catch((err) => {})
+
     S_name = req.body.Name;
     S_id = req.body.ST_ID;
 
