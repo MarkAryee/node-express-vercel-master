263b53dd3f88b8917c8ad576b102ba6a61d71853
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-18 15:58:30 +0000

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -6,7 +6,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 
 ///*
-const doURI = "mongodb+srv://vercel-admin-user:rasengan@cluster0.ad3usrv.mongodb.net/?retryWrites=true&w=majority"
+const doURI = "mongodb+srv://vercel-admin-user:rasenshuriken884@cluster0.ad3usrv.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(doURI, {useNewUrlParser: true, useUnifiedTopology: true}).then((result) => {
 // app.listen(3000);
 }).catch((err) => {
